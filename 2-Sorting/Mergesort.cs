f6ef38c69898e903ac2885e17439a25e8daa331e
Yannick Willems <ywillems96@gmail.com>
2020-09-17 14:33:29 +0200

    WIP: Mergesort

--- 2-Sorting/Mergesort.cs
+++ 2-Sorting/Mergesort.cs
@@ -10,21 +10,29 @@ namespace ALGA
     {
         public static void mergesort(ISortList list)
         {
-           if(list.Count <= 1)
-           {
-                return;
-           }
+           if(list.Count <= 1) return;
 
-            SortList x = new SortList();
-            SortList y = new SortList();
+            SortList left = new SortList();
+            SortList right = new SortList();
+
             int mid = list.Count / 2;
 
             // Dividing the list
             for (int n = 0; n < mid; n++)
             {
-                y[n] = list[n];
+                left[n] = list[n];
             }
+            for(int n = mid; n < list.Count; n++)
+            {
+                right[n] = list[n];
+            }
+
+            left = mergesort(left);
+            right = mergesort(right);
+
+
         }
+
         public static void mergesort(ISortList list, int leftIndex, int rightIndex)
         {
             throw new NotImplementedException();
