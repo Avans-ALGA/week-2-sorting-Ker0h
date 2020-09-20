fb7fcf82a4a0fa5ed70fbb1264a97096dd2ce42b
Ker0h <ywillems@protonmail.com>
2020-09-20 07:57:53 -0700

    Mergesort done :)

--- 2-Sorting-Runnable/Program.cs
+++ 2-Sorting-Runnable/Program.cs
@@ -11,8 +11,8 @@ namespace ALGA
         static void Main(string[] args)
         {
             SortList list = new SortList(50);
-            Quicksort.quicksort(list);
-            // Mergesort.mergesort(list);
+            // Quicksort.quicksort(list);
+            Mergesort.mergesort(list);
             Console.WriteLine("Result: " + list);
             Console.ReadLine();
         }
--- 2-Sorting/Mergesort.cs
+++ 2-Sorting/Mergesort.cs
@@ -18,38 +18,76 @@ namespace ALGA
         public static void mergesort(ISortList list, int leftIndex, int rightIndex)
         {
             Console.WriteLine(list.ToString());
-            divide(list);
-        }
-
-        public static ISortList divide(ISortList list)
-        {
-            if (list.Count <= 1) return list;
-
-            ISortList left = new SortList();
-            ISortList right = new SortList();
-
-            int middle = list.Count / 2;
-
-            for (int i = 0; i < middle; i++)
-            {
-                left[i] = list[i];
-            }
-
-            for (int i = middle; i < list.Count; i++)
-            {
-                right[i] = list[i];
-            }
-
-            left = divide(left);
-            right = divide(right);
-
-            Console.WriteLine(left.ToString());
-            return null;
-        }
-
-        public static void merge(ISortList left, ISortList right)
-        {
             
+            if(leftIndex < rightIndex)
+            {
+                int middleIndex = (leftIndex + rightIndex) / 2;
+
+                // Divide both halves
+                mergesort(list, leftIndex, middleIndex);
+                mergesort(list, middleIndex + 1, rightIndex);
+
+                merge(list, leftIndex, middleIndex, rightIndex);
+            }
+        }
+
+
+        public static void merge(ISortList list, int leftIndex, int middleIndex, int rightIndex)
+        {
+            int leftSubIndex, rightSubIndex, mergedSubindex;
+            int leftSubSize = middleIndex - leftIndex + 1;
+            int rightSubSize = rightIndex - middleIndex;
+
+            // Temporary lists
+            ISortList left = new SortList(leftSubSize);
+            ISortList right = new SortList(rightSubSize);
+
+            // Copy data into temp. lists
+            for (leftSubIndex = 0; leftSubIndex < leftSubSize; leftSubIndex++)
+            {
+                left[leftSubIndex] = list[leftIndex + leftSubIndex];
+            }
+
+            for (rightSubIndex = 0; rightSubIndex < rightSubSize; rightSubIndex++)
+            {
+                right[rightSubIndex] = list[middleIndex + 1 + rightSubIndex];
+            }
+
+            // Merge temp. lists  back into original
+            leftSubIndex = 0;
+            rightSubIndex = 0;
+            mergedSubindex = leftIndex;
+
+            while (leftSubIndex < leftSubSize && rightSubIndex < rightSubSize)
+            {
+                if(left[leftSubIndex] <= right[rightSubIndex])
+                {
+                    list[mergedSubindex] = left[leftSubIndex];
+                    leftSubIndex++;
+                }
+                else
+                {
+                    list[mergedSubindex] = right[rightSubIndex];
+                    rightSubIndex++;
+                }
+                mergedSubindex++;
+            }
+
+            // Copy any remainders from the left list
+            while(leftSubIndex < leftSubSize)
+            {
+                list[mergedSubindex] = left[leftSubIndex];
+                leftSubIndex++;
+                mergedSubindex++;
+            }
+
+            // Copy any remainders from the right list
+            while (rightSubIndex < rightSubSize)
+            {
+                list[mergedSubindex] = right[rightSubIndex];
+                rightSubIndex++;
+                mergedSubindex++;
+            }
         }
     }
 }
